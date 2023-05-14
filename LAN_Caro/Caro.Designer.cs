446b66628b71821308480d80e973bfc725ffda19
Phúc Đặng Nguyễn Hoàng <21522470@ms.uit.edu.vn>
2023-05-14 14:25:43 +0700

    Add project files.

---
+++ LAN_Caro/Caro.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace LAN_Caro
+{
+    partial class Caro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnTable = new Lab_3.Addon_Round_Panel();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // pnTable
+            // 
+            pnTable.BackColor = Color.White;
+            pnTable.CornerRadius = 25;
+            pnTable.Location = new Point(286, 32);
+            pnTable.Name = "pnTable";
+            pnTable.Size = new Size(996, 711);
+            pnTable.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = SystemColors.ActiveCaptionText;
+            button1.Image = Properties.Resources.o;
+            button1.Location = new Point(90, 208);
+            button1.Name = "button1";
+            button1.Size = new Size(40, 40);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Caro
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(247, 249, 252);
+            ClientSize = new Size(1312, 770);
+            Controls.Add(button1);
+            Controls.Add(pnTable);
+            Name = "Caro";
+            Text = "Caro";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Lab_3.Addon_Round_Panel pnTable;
+        private Button button1;
+    }
+}
---
+++ LAN_Caro/TableManager.cs
@@ -0,0 +1,72 @@
+﻿using Lab_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAN_Caro
+{
+    public class TableManager
+    {
+        private Addon_Round_Panel table;
+        public Addon_Round_Panel Table { get => table; set => table = value; }
+        public List<Player> PlayerList { get; set; }
+        private int PlayerNum;
+        public TableManager(Addon_Round_Panel table)
+        {
+            this.Table = table;
+            this.PlayerList = new List<Player>()
+            {
+                new Player("X", Properties.Resources.x),
+                new Player("O", Properties.Resources.o)
+            };
+            PlayerNum = 0;
+        }
+
+
+
+
+        public void DrawTable()
+        {
+            Button oldButton = new Button() { Width = 0, Location = new Point(40, 40) };
+            for (int i = 0; i < Const.TABLE_HEIGHT; i++)
+            {
+                for (int j = 0; j < Const.TABLE_WIDTH; j++)
+                {
+                    Button button = new Button()
+                    {
+                        Width = Const.SQUARE_SIZE,
+                        Height = Const.SQUARE_SIZE,
+                        Location = new Point(oldButton.Location.X + oldButton.Width, oldButton.Location.Y),
+                        FlatStyle = FlatStyle.Flat,
+
+                    };
+                    button.FlatAppearance.BorderColor = Color.Silver;
+                    button.Click += btn_Click;
+                    Table.Controls.Add(button);
+                    oldButton = button;
+                }
+                oldButton.Location = new Point(40, oldButton.Location.Y + Const.SQUARE_SIZE);
+                oldButton.Width = 0;
+                oldButton.Height = 0;
+            }
+
+        }
+
+        void btn_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            if (button.Image != null) 
+                return;
+            button.Image = PlayerList[PlayerNum].Sign;
+            if (PlayerNum == 0)
+            {
+                PlayerNum = 1;
+            }
+            else PlayerNum = 0;
+        }
+
+
+    }
+}
