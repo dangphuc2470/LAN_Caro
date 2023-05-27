67fbe99bec6336e6832a49728fc6c386b1a8ab4a
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2023-05-27 15:27:27 +0700

    Xong luat choi

--- LAN_Caro/TableManager.cs
+++ LAN_Caro/TableManager.cs
@@ -1,7 +1,9 @@
 ﻿using Lab_3;
 using System;
 using System.Collections.Generic;
+using System.Formats.Asn1;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +11,14 @@ namespace LAN_Caro
 {
     public class TableManager
     {
+        public static int SQUARE_SIZE = 40;
+        public static int TABLE_WIDTH = 24;
+        public static int TABLE_HEIGHT = 16;
         private Addon_Round_Panel table;
         public Addon_Round_Panel Table { get => table; set => table = value; }
         public List<Player> PlayerList { get; set; }
+        public List<List<Button>> buttonList = new List<List<Button>>();
+
         private int PlayerNum;
         public TableManager(Addon_Round_Panel table)
         {
@@ -30,24 +37,28 @@ namespace LAN_Caro
         public void DrawTable()
         {
             Button oldButton = new Button() { Width = 0, Location = new Point(40, 40) };
-            for (int i = 0; i < Const.TABLE_HEIGHT; i++)
+            for (int i = 0; i < TABLE_HEIGHT; i++)
             {
-                for (int j = 0; j < Const.TABLE_WIDTH; j++)
+                buttonList.Add(new List<Button>());
+                for (int j = 0; j < TABLE_WIDTH; j++)
                 {
                     Button button = new Button()
                     {
-                        Width = Const.SQUARE_SIZE,
-                        Height = Const.SQUARE_SIZE,
+                        Width = SQUARE_SIZE,
+                        Height = SQUARE_SIZE,
                         Location = new Point(oldButton.Location.X + oldButton.Width, oldButton.Location.Y),
                         FlatStyle = FlatStyle.Flat,
+                        Name = i.ToString() //Name as Y
 
                     };
+                    button.Tag = j;         //Tag as X
                     button.FlatAppearance.BorderColor = Color.Silver;
                     button.Click += btn_Click;
                     Table.Controls.Add(button);
+                    buttonList[i].Add(button);
                     oldButton = button;
                 }
-                oldButton.Location = new Point(40, oldButton.Location.Y + Const.SQUARE_SIZE);
+                oldButton.Location = new Point(40, oldButton.Location.Y + SQUARE_SIZE);
                 oldButton.Width = 0;
                 oldButton.Height = 0;
             }
@@ -57,16 +68,116 @@ namespace LAN_Caro
         void btn_Click(object sender, EventArgs e)
         {
             Button button = sender as Button;
-            if (button.Image != null) 
+            int y = Int32.Parse(button.Name);
+            int x = Int32.Parse(button.Tag.ToString());
+            if (buttonList[y][x].Image != null)
                 return;
-            button.Image = PlayerList[PlayerNum].Sign;
+            buttonList[y][x].Image = PlayerList[PlayerNum].Sign;
+
+
+
+
+            if (countVertical(button) >=4 || countHorizontal(button) >= 4 || countMainDiag(button) >=4 || countSubDiag(button) >= 4)
+                MessageBox.Show("WIN");
+
+
             if (PlayerNum == 0)
             {
                 PlayerNum = 1;
             }
             else PlayerNum = 0;
         }
+        public int countVertical(Button button)
+        {
+            int y = Int32.Parse(button.Name);
+            int x = Int32.Parse(button.Tag.ToString());
+            int count = 0;
+            for (int i = x - 1; i >= 0; i--)
+            {
+                if (buttonList[y][i].Image == buttonList[y][x].Image)
+                    count++;
+                else
+                    break;
+            }
+
+            for (int i = x + 1; i < TABLE_WIDTH; i++)
+            {
+                if (buttonList[y][i].Image == buttonList[y][x].Image)
+                    count++;
+                else
+                    break;
+            }
+            return count;
+
+
+        }
+        public int countHorizontal(Button button)
+        {
+            int y = Int32.Parse(button.Name);
+            int x = Int32.Parse(button.Tag.ToString());
+            int count = 0;
+            for (int i = y - 1; i >= 0; i--)
+            {
+                if (buttonList[i][x].Image == buttonList[y][x].Image)
+                    count++;
+                else
+                    break;
+            }
+            for (int i = y + 1; i < TABLE_HEIGHT; i++)
+            {
+                if (buttonList[i][x].Image == buttonList[y][x].Image)
+                    count++;
+                else
+                    break;
+            }
+
+            return count;
+
+        }
+        public int countMainDiag(Button button)
+        {
+            int y = Int32.Parse(button.Name);
+            int x = Int32.Parse(button.Tag.ToString());
+            int count = 0;
+            for (int i = x - 1, j = y - 1; i >= 0 && j >= 0; i--, j--)
+            {
+                if (buttonList[j][i].Image == buttonList[y][x].Image)
+                    count++;
+                else break;
+            }
+            for (int i = x + 1, j = y + 1; i < TABLE_WIDTH && j < TABLE_HEIGHT; i++, j++)
+            {
+                if (buttonList[j][i].Image == buttonList[y][x].Image)
+                    count++;
+                else break;
+            }
+            return count;
+        }
+
+        public int countSubDiag(Button button)
+        {
+            int y = Int32.Parse(button.Name);
+            int x = Int32.Parse(button.Tag.ToString());
+            int count = 0;
+            for (int i = x - 1, j = y + 1; i >= 0 && j < TABLE_HEIGHT; i--, j++)
+            {
+                if (buttonList[j][i].Image == buttonList[y][x].Image)
+                    count++;
+                else break;
+            }
+            for (int i = x + 1, j = y - 1; i < TABLE_WIDTH && j >= 0; i++, j--)
+            {
+                if (buttonList[j][i].Image == buttonList[y][x].Image)
+                    count++;
+                else break;
+            }
+            return count;
+        }
+
+
+
 
 
     }
 }
+
