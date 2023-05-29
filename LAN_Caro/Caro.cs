7e71cd54e508f81d0516f108029f1c4aa7f7c02e
dangphuc2470 <dangnguyenhoangphuc1@gmail.com>
2023-05-29 19:57:48 +0700

    Chơi đc.

--- LAN_Caro/Caro.cs
+++ LAN_Caro/Caro.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,13 +13,71 @@ namespace LAN_Caro
 {
     public partial class Caro : Form
     {
+
+        int isServerPlayer = 0;
+        TableManager tableManager;
+
+        Server_OBJ serverObj;
+        Client_OBJ clientObj;
         public Caro()
         {
             InitializeComponent();
-            TableManager tableManager = new TableManager(pnTable);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            tableManager.switchPlayer();
+        }
+
+        private void Caro_Load(object sender, EventArgs e)
+        {
+            tableManager = new TableManager(pnTable);
+            tableManager.ButtonClicked += TableManager_ButtonClicked;
             tableManager.DrawTable();
         }
 
+        private void btConnect_Click(object sender, EventArgs e)
+        {
+            if (chbServer.CheckState == CheckState.Checked)
+            {
+                serverObj = new Server_OBJ(tableManager);
+                isServerPlayer = 1;
+                tableManager.setPlayer(isServerPlayer);
+            }
+            else
+            {
+                clientObj = new Client_OBJ(tableManager, tbIPAdress.Text);
+            }
 
+        }
+
+        private void btSend_Click(object sender, EventArgs e)
+        {
+            if (isServerPlayer == 1)
+            {
+                serverObj.sendToClient(tbData.Text + "\n");
+                tableManager.switchPlayer();
+            }
+            else
+            {
+                clientObj.messageSend(tbData.Text + "\n");
+                tableManager.switchPlayer();
+            }
+
+        }
+        private void TableManager_ButtonClicked(object sender, string text)
+        {
+            if (isServerPlayer == 1)
+            {
+                serverObj.sendToClient(text);
+                tableManager.switchPlayer();
+            }
+            else
+            {
+                clientObj.messageSend(text);
+                tableManager.switchPlayer();
+            }
+        }
     }
 }
+
