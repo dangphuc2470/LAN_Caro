7ebe88e7601f6eefc9c3f666eb8a52f088e1c1db
Phúc Đặng Nguyễn Hoàng <21522470@ms.uit.edu.vn>
2023-06-06 22:51:42 +0700

    Xong Main Menu

---
+++ LAN_Caro/MenuForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace LAN_Caro
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuForm));
+            pnStartMenu = new Panel();
+            btPlaySingle = new Addon_Custom_Button();
+            lbAbout = new CustomLabel();
+            lbSetting = new CustomLabel();
+            lbHelp = new CustomLabel();
+            btLan = new Addon_Custom_Button();
+            ptbBackround = new PictureBox();
+            pnMultiplayer = new Panel();
+            pnStartMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)ptbBackround).BeginInit();
+            SuspendLayout();
+            // 
+            // pnStartMenu
+            // 
+            pnStartMenu.Controls.Add(btPlaySingle);
+            pnStartMenu.Controls.Add(lbAbout);
+            pnStartMenu.Controls.Add(lbSetting);
+            pnStartMenu.Controls.Add(lbHelp);
+            pnStartMenu.Controls.Add(btLan);
+            pnStartMenu.Controls.Add(ptbBackround);
+            pnStartMenu.Dock = DockStyle.Fill;
+            pnStartMenu.Location = new Point(0, 0);
+            pnStartMenu.Name = "pnStartMenu";
+            pnStartMenu.Size = new Size(1480, 788);
+            pnStartMenu.TabIndex = 20;
+            pnStartMenu.VisibleChanged += pnStartMenu_VisibleChanged;
+            // 
+            // btPlaySingle
+            // 
+            btPlaySingle.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btPlaySingle.BackColor = Color.MediumSlateBlue;
+            btPlaySingle.BackgroundColor = Color.MediumSlateBlue;
+            btPlaySingle.BackgroundImage = Properties.Resources.PSFix_20230605_223910_auto_x2_3;
+            btPlaySingle.BackgroundImageLayout = ImageLayout.Stretch;
+            btPlaySingle.BorderColor = Color.White;
+            btPlaySingle.BorderRadius = 4;
+            btPlaySingle.BorderSize = 1;
+            btPlaySingle.FlatAppearance.BorderSize = 0;
+            btPlaySingle.FlatStyle = FlatStyle.Flat;
+            btPlaySingle.ForeColor = Color.White;
+            btPlaySingle.Location = new Point(38, 578);
+            btPlaySingle.Name = "btPlaySingle";
+            btPlaySingle.Size = new Size(329, 86);
+            btPlaySingle.TabIndex = 20;
+            btPlaySingle.Text = "PLAY";
+            btPlaySingle.TextColor = Color.White;
+            btPlaySingle.UseVisualStyleBackColor = false;
+            btPlaySingle.Click += btPlayMM_Click;
+            // 
+            // lbAbout
+            // 
+            lbAbout.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lbAbout.AutoSize = true;
+            lbAbout.BackColor = Color.Transparent;
+            lbAbout.FlatStyle = FlatStyle.Flat;
+            lbAbout.ForeColor = Color.White;
+            lbAbout.Location = new Point(636, 708);
+            lbAbout.Name = "lbAbout";
+            lbAbout.Size = new Size(69, 25);
+            lbAbout.TabIndex = 24;
+            lbAbout.Text = "ABOUT";
+            // 
+            // lbSetting
+            // 
+            lbSetting.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lbSetting.AutoSize = true;
+            lbSetting.BackColor = Color.Transparent;
+            lbSetting.FlatStyle = FlatStyle.Flat;
+            lbSetting.ForeColor = Color.White;
+            lbSetting.Location = new Point(465, 708);
+            lbSetting.Name = "lbSetting";
+            lbSetting.Size = new Size(79, 25);
+            lbSetting.TabIndex = 23;
+            lbSetting.Text = "SETTING";
+            // 
+            // lbHelp
+            // 
+            lbHelp.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            lbHelp.AutoSize = true;
+            lbHelp.BackColor = Color.Transparent;
+            lbHelp.FlatStyle = FlatStyle.Flat;
+            lbHelp.ForeColor = Color.White;
+            lbHelp.Location = new Point(783, 708);
+            lbHelp.Name = "lbHelp";
+            lbHelp.Size = new Size(52, 25);
+            lbHelp.TabIndex = 22;
+            lbHelp.Text = "HELP";
+            // 
+            // btLan
+            // 
+            btLan.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btLan.BackColor = Color.MediumSlateBlue;
+            btLan.BackgroundColor = Color.MediumSlateBlue;
+            btLan.BackgroundImage = Properties.Resources.PSFix_20230605_223910_auto_x2_2;
+            btLan.BackgroundImageLayout = ImageLayout.Stretch;
+            btLan.BorderColor = Color.White;
+            btLan.BorderRadius = 4;
+            btLan.BorderSize = 1;
+            btLan.FlatAppearance.BorderSize = 0;
+            btLan.FlatStyle = FlatStyle.Flat;
+            btLan.ForeColor = Color.White;
+            btLan.Location = new Point(38, 670);
+            btLan.Name = "btLan";
+            btLan.Size = new Size(329, 86);
+            btLan.TabIndex = 21;
+            btLan.Text = "MULTIPLAYER";
+            btLan.TextColor = Color.White;
+            btLan.UseVisualStyleBackColor = false;
+            btLan.Click += btLan_Click;
+            // 
+            // ptbBackround
+            // 
+            ptbBackround.Dock = DockStyle.Fill;
+            ptbBackround.Image = Properties.Resources.backround;
+            ptbBackround.Location = new Point(0, 0);
+            ptbBackround.Name = "ptbBackround";
+            ptbBackround.Size = new Size(1480, 788);
+            ptbBackround.SizeMode = PictureBoxSizeMode.StretchImage;
+            ptbBackround.TabIndex = 0;
+            ptbBackround.TabStop = false;
+            // 
+            // pnMultiplayer
+            // 
+            pnMultiplayer.BackgroundImage = Properties.Resources.fakeBackground;
+            pnMultiplayer.BackgroundImageLayout = ImageLayout.Stretch;
+            pnMultiplayer.Dock = DockStyle.Fill;
+            pnMultiplayer.Location = new Point(0, 0);
+            pnMultiplayer.Name = "pnMultiplayer";
+            pnMultiplayer.Size = new Size(1480, 788);
+            pnMultiplayer.TabIndex = 21;
+            pnMultiplayer.Visible = false;
+            pnMultiplayer.ControlRemoved += pnMultiplayer_ControlRemoved;
+            // 
+            // MenuForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1480, 788);
+            Controls.Add(pnStartMenu);
+            Controls.Add(pnMultiplayer);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            KeyPreview = true;
+            Name = "MenuForm";
+            Text = "Paper & Pencil";
+            ResizeEnd += MenuForm_SizeChanged;
+            KeyDown += MenuForm_KeyDown;
+            Move += MenuForm_Move;
+            pnStartMenu.ResumeLayout(false);
+            pnStartMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)ptbBackround).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnStartMenu;
+        private PictureBox ptbBackround;
+        private Addon_Custom_Button btPlaySingle;
+        private CustomLabel lbAbout;
+        private CustomLabel lbSetting;
+        private CustomLabel lbHelp;
+        private Addon_Custom_Button btLan;
+        private Panel pnMultiplayer;
+    }
+}
---
+++ LAN_Caro/MenuForm.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAN_Caro
+{
+    public partial class MenuForm : Form
+    {
+        private Form activeForm = null;
+        public int resizeCount = 0;
+        Caro caro;
+        private void openChildForm(Form childForm)
+        {
+            if (activeForm != null) activeForm.Close();
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            pnMultiplayer.Controls.Add(childForm);
+            pnMultiplayer.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+
+        }
+
+        public MenuForm()
+        {
+            InitializeComponent();
+            #region test
+            //pnStartMenu.Visible = false;
+            //pnMultiplayer.Visible = true;
+            #endregion
+            #region Format StartMenu Panel
+            btPlaySingle.UseCustomFont("UI.ttf", 35, FontStyle.Regular);
+            btLan.UseCustomFont("UI.ttf", 35, FontStyle.Regular);
+
+
+            lbHelp.Parent = ptbBackround;
+            lbHelp.BackColor = Color.Transparent;
+            lbHelp.ForeColor = Color.White;
+            lbHelp.UseCustomFont("UI.ttf", 25, FontStyle.Bold);
+            lbHelp.Visible = true;
+
+            lbAbout.Parent = ptbBackround;
+            lbAbout.BackColor = Color.Transparent;
+            lbAbout.ForeColor = Color.White;
+            lbAbout.UseCustomFont("UI.ttf", 25, FontStyle.Bold);
+            lbAbout.Visible = true;
+
+            lbSetting.Parent = ptbBackround;
+            lbSetting.BackColor = Color.Transparent;
+            lbSetting.ForeColor = Color.White;
+            lbSetting.UseCustomFont("UI.ttf", 25, FontStyle.Bold);
+            lbSetting.Visible = true;
+            #endregion
+        }
+
+        #region MenuForm Event
+        private void MenuForm_SizeChanged(object sender, EventArgs e)
+        {
+
+            int width = this.Width;
+            int height = this.Height;
+            int targetHeight = (int)(width * 9.0 / 16.0);
+
+            if (height != targetHeight)
+            {
+                this.SetBounds(this.Bounds.X, this.Bounds.Y, width, targetHeight);
+            }
+
+        }
+
+        /// <summary>
+        /// Chuyển backround thành ảnh tĩnh khi người dùng di chuyển form để tối ưu hiệu năng
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MenuForm_Move(object sender, EventArgs e)
+        {
+            if (resizeCount == 0) //Khi khởi tạo thì form sẽ kích hoạt sự kiện này một lần, nên cần phải bỏ qua
+            {
+                caro = new Caro();
+                openChildForm(caro); ///Vì khi khởi tạo thì sự kiện này sẽ kích hoạt 1 lần nên ta tân dụng nó để load game trước trong nền
+                resizeCount++;
+                return;
+            }
+            else if (resizeCount == 1)
+            {
+                ptbBackround.Image = Properties.Resources.Backgroud;
+                resizeCount++;
+            }
+        }
+
+        private void MenuForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                if (pnMultiplayer.Visible)
+                    caro.ShowPausePanel();
+            }
+        }
+        #endregion
+        private void btLan_Click(object sender, EventArgs e)
+        {
+            pnStartMenu.Visible = false;
+            pnMultiplayer.Visible = true;
+            caro.LoadButtonColor();
+
+        }
+
+
+        private void btPlayMM_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pnMultiplayer_ControlRemoved(object sender, ControlEventArgs e)
+        {
+            pnStartMenu.Visible = true;
+            caro.Dispose();
+            caro = new Caro();
+            openChildForm(caro);
+        }
+
+
+        /// <summary>
+        /// Vì font chữ custom gây ra 1 exception khi random pattern và leave match nên khi start menu bị ẩn thì đổi font chữ lại bình thường
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void pnStartMenu_VisibleChanged(object sender, EventArgs e)
+        {
+            if (!pnStartMenu.Visible)
+            {
+                btPlaySingle.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+                btLan.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+                lbSetting.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+                lbHelp.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+                lbAbout.Font = new Font("Segoe UI", 9, FontStyle.Regular);
+            }
+            else
+            {
+
+                btPlaySingle.UseCustomFont("UI.ttf", 35, FontStyle.Regular);
+                btLan.UseCustomFont("UI.ttf", 35, FontStyle.Regular);
+
+                lbSetting.UseCustomFont("UI.ttf", 25, FontStyle.Bold);
+                lbHelp.UseCustomFont("UI.ttf", 25, FontStyle.Bold);
+                lbAbout.UseCustomFont("UI.ttf", 25, FontStyle.Bold);
+
+            }
+
+        }
+    }
+}
